44374678e571968b232090c1ae7e064a60f7166d
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2019-05-07 15:32:46 +0800

    Update keras_model.py

--- keras_model.py
+++ keras_model.py
@@ -2,7 +2,7 @@
 # The SELDnet architecture
 #
 
-from keras.layers import Bidirectional, Conv2D, MaxPooling2D, Input
+from keras.layers import Bidirectional, Conv2D, MaxPooling2D, Input, Lambda
 from keras.layers.core import Dense, Activation, Dropout, Reshape, Permute
 from keras.layers.recurrent import GRU
 from keras.layers.normalization import BatchNormalization
@@ -21,6 +21,7 @@ def get_model(data_in, data_out, dropout_rate, nb_cnn2d_filt, pool_size,
     filters_list = [32,64,128]
     # CNN
     spec_cnn = spec_start
+
     for i, convCnt in enumerate(pool_size):
         spec_cnn = Conv2D(filters=filters_list[i], kernel_size=(3, 3), padding='same')(spec_cnn)
         spec_cnn = BatchNormalization()(spec_cnn)
@@ -34,26 +35,28 @@ def get_model(data_in, data_out, dropout_rate, nb_cnn2d_filt, pool_size,
     #spec_rnn = Reshape((data_in[-2], -1))(spec_cnn)
     for j in range(5):
        for nb_kernels in [1,3,1]:
-          spec_cnn = Conv2D( filters =128, kernel_size=nb_kernel, padding='same'
+          spec_cnn = Conv2D( filters =128, kernel_size=nb_kernels, padding='same'
           )(spec_cnn) # Outputs = B, 128, 128, 4
     
     doa = spec_cnn
     sed =spec_cnn
     
-    doa = Conv2D(filters=22, kernel_size = 1, padding= 'same') # B, 22, 128, 4
-    sed = Conv2D(filters=11, kernel_size = 1, padding= 'same') # B, 11, 128, 4
+    doa = Conv2D(filters=22, kernel_size = 1, padding= 'same')(doa) # B, 22, 128, 4
+    sed = Conv2D(filters=11, kernel_size = 1, padding= 'same')(sed) # B, 11, 128, 4
         
     doa = Permute((3,2,1))(doa) # B, 4, 128, 22
     sed = Permute((3,2,1))(sed) # B, 4, 128, 11
     
-    doa  = Conv2D(filters=1, padding='same')(doa) # B, 1, 128, 22
-    sed  = Conv2D(filters=1, padding='same')(sed) # B, 1, 128, 11
+    doa  = Conv2D(filters=1, kernel_size =1, padding='same')(doa) # B, 1, 128, 22
+    sed  = Conv2D(filters=1, kernel_size =1, padding='same')(sed) # B, 1, 128, 11
+
+    doa = Lambda(lambda y: keras.backend.squeeze(y,axis=1))(doa) # Using Lambda as a workaround to keras bug
+    sed = Lambda(lambda y: keras.backend.squeeze(y,axis=1))(sed) # Allows us to use squeeze and return a keras tensor
 
     doa = Activation('linear', name='doa_out')(doa)
     sed = Activation('sigmoid', name='sed_out')(sed)
     
-    doa = np.squeeze(doa,axis=1)
-    sed = np.squeeze(sed,axis=1)
+
     model = Model(inputs=spec_start, outputs=[sed, doa])
     model.compile(optimizer=Adam(), loss=['binary_crossentropy', 'mse'], loss_weights=weights)
 
