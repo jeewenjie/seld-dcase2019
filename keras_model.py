345d70b802297a7f52722852c12ba218e4b0e5af
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2019-05-07 11:16:14 +0800

    Update keras_model.py

--- keras_model.py
+++ keras_model.py
@@ -18,42 +18,39 @@ def get_model(data_in, data_out, dropout_rate, nb_cnn2d_filt, pool_size,
                                 rnn_size, fnn_size, weights):
     # model definition
     spec_start = Input(shape=(data_in[-3], data_in[-2], data_in[-1]))
-
+    filters_list = [32,64,128]
     # CNN
     spec_cnn = spec_start
     for i, convCnt in enumerate(pool_size):
-        spec_cnn = Conv2D(filters=nb_cnn2d_filt, kernel_size=(3, 3), padding='same')(spec_cnn)
+        spec_cnn = Conv2D(filters=filters_list[i], kernel_size=(3, 3), padding='same')(spec_cnn)
         spec_cnn = BatchNormalization()(spec_cnn)
         spec_cnn = Activation('relu')(spec_cnn)
         spec_cnn = MaxPooling2D(pool_size=(1, pool_size[i]))(spec_cnn)
         spec_cnn = Dropout(dropout_rate)(spec_cnn)
-    spec_cnn = Permute((2, 1, 3))(spec_cnn)
+    #spec_cnn = Permute((2, 1, 3))(spec_cnn)
+    
 
-    # RNN
-    spec_rnn = Reshape((data_in[-2], -1))(spec_cnn)
-    for nb_rnn_filt in rnn_size:
-        spec_rnn = Bidirectional(
-            GRU(nb_rnn_filt, activation='tanh', dropout=dropout_rate, recurrent_dropout=dropout_rate,
-                return_sequences=True),
-            merge_mode='mul'
-        )(spec_rnn)
+    # More CNN
+    #spec_rnn = Reshape((data_in[-2], -1))(spec_cnn)
+    for j in range(5):
+       for nb_kernels in [1,3,1]:
+          spec_cnn = Conv2D( filters =128, kernel_size=nb_kernel, padding='same'
+          )(spec_cnn) # Outputs = B, 128, 128, 4
+    
+    doa = spec_cnn
+    sed =spec_cnn
+    
+    doa = Conv2D(filters=22, kernel_size = 1, padding= 'same') # B, 22, 128, 4
+    sed = Conv2D(filters=11, kernel_size = 1, padding= 'same') # B, 11, 128, 4
+        
+    doa = Permute((3,2,1))(doa) # B, 4, 128, 22
+    sed = Permute((3,2,1))(sed) # B, 4, 128, 11
+    
+    doa  = Conv2D(filters=1, padding='same')(doa) # B, 1, 128, 22
+    sed  = Conv2D(filters=1, padding='same')(sed) # B, 1, 128, 11
 
-    # FC - DOA
-    doa = spec_rnn
-    for nb_fnn_filt in fnn_size:
-        doa = TimeDistributed(Dense(nb_fnn_filt))(doa)
-        doa = Dropout(dropout_rate)(doa)
-
-    doa = TimeDistributed(Dense(data_out[1][-1]))(doa)
-    doa = Activation('linear', name='doa_out')(doa)
-
-    # FC - SED
-    sed = spec_rnn
-    for nb_fnn_filt in fnn_size:
-        sed = TimeDistributed(Dense(nb_fnn_filt))(sed)
-        sed = Dropout(dropout_rate)(sed)
-    sed = TimeDistributed(Dense(data_out[0][-1]))(sed)
-    sed = Activation('sigmoid', name='sed_out')(sed)
+    doa = Activation('linear', name='doa_out')(doa)[0,2,3]
+    sed = Activation('sigmoid', name='sed_out')(sed)[0,2,3]
 
     model = Model(inputs=spec_start, outputs=[sed, doa])
     model.compile(optimizer=Adam(), loss=['binary_crossentropy', 'mse'], loss_weights=weights)
